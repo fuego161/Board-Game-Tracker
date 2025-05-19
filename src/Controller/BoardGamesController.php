78a13bdfd369833ed09f3deea8b677dfedb97018
fuego161 <46138241+fuego161@users.noreply.github.com>
2025-05-19 22:37:04 +0100

    Add View BoardGame action

--- src/Controller/BoardGamesController.php
+++ src/Controller/BoardGamesController.php
@@ -10,4 +10,11 @@ class BoardGamesController extends AppController
 
         $this->set(compact('boardGames'));
     }
+
+    public function view($slug = null)
+    {
+        $boardGame = $this->BoardGames->findBySlug($slug)->firstOrFail();
+
+        $this->set(compact('boardGame'));
+    }
 }
---
+++ templates/BoardGames/view.php
@@ -0,0 +1,7 @@
+<h1><?= h($boardGame->title); ?></h1>
+<h3>Published by: <?= h($boardGame->publisher); ?></h3>
+<p>Player Count: <?= h($boardGame->min_players) . ' - ' . h($boardGame->max_players); ?></p>
+
+<hr>
+
+<p>Add to your collection!</p>
