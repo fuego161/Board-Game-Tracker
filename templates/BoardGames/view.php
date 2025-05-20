9c9b7914a1b83454e7fa9f44e131610a0bfe1e5c
fuego161 <46138241+fuego161@users.noreply.github.com>
2025-05-20 22:03:22 +0100

    Update BoardGames view to match baked Categories

--- templates/BoardGames/view.php
+++ templates/BoardGames/view.php
@@ -1,31 +1,45 @@
-<h1><?= h($boardGame->title); ?></h1>
-<h3>Published by: <?= h($boardGame->publisher); ?></h3>
-<p><?= h($boardGame->category_string); ?></p>
-<p>Player Count: <?= h($boardGame->min_players) . ' - ' . h($boardGame->max_players); ?></p>
+<div class="row">
 
-<hr>
-<!-- TODO: Link to a users collection -->
-<!-- TODO: Only show if not within logged in users collection -->
-<!-- TODO: Redirect to log in if not logged in -->
-<?= $this->Html->link(
-    'Add to your collection!',
-    ['action' => 'add'],
-    ['class' => 'button']
-); ?>
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('Edit Board Game'), ['action' => 'edit', $boardGame->slug], ['class' => 'side-nav-item']) ?>
+            <?= $this->Form->postLink(__('Delete Board Game'), ['action' => 'delete', $boardGame->slug], ['confirm' => __('Are you sure you want to delete # {0}?', $boardGame->slug), 'class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('Add New Board Game'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('See All Board Games'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
 
-<!-- TODO: Make admin only -->
-<?= $this->Html->link(
-    'Edit Game',
-    ['action' => 'edit', $boardGame->slug],
-    ['class' => 'button button-outline']
-); ?>
+    <div class="column column-80">
+        <div class="board-games view content">
+            <h3><?= h($boardGame->title) ?></h3>
+            <table>
+                <tr>
+                    <th><?= __('Title') ?></th>
+                    <td><?= h($boardGame->title) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Publisher') ?></th>
+                    <td><?= h($boardGame->title) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Player Count') ?></th>
+                    <td><?=
+                            $boardGame->min_players === $boardGame->max_players ?
+                            h($boardGame->min_players) :
+                            h($boardGame->min_players) . ' - ' . h($boardGame->max_players); ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th><?= __('Created') ?></th>
+                    <td><?= h($boardGame->created) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Modified') ?></th>
+                    <td><?= h($boardGame->modified) ?></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
 
-<!-- TODO: Make admin only -->
-<?= $this->Form->deleteLink(
-    'Delete',
-    ['action' => 'delete', $boardGame->slug],
-    [
-        'class' => 'button button-outline',
-        'confirm' => 'Are you sure?',
-    ]
-); ?>
