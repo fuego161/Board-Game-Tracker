21319470a62ca4bf4c8be751974f5e568b81947b
fuego161 <46138241+fuego161@users.noreply.github.com>
2025-05-20 17:26:02 +0100

    Update category index table view

--- templates/Categories/index.php
+++ templates/Categories/index.php
@@ -11,22 +11,17 @@
         <table>
             <thead>
                 <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
                     <th><?= $this->Paginator->sort('title') ?></th>
-                    <th><?= $this->Paginator->sort('created') ?></th>
-                    <th><?= $this->Paginator->sort('modified') ?></th>
                     <th class="actions"><?= __('Actions') ?></th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($categories as $category): ?>
                 <tr>
-                    <td><?= $this->Number->format($category->id) ?></td>
                     <td><?= h($category->title) ?></td>
-                    <td><?= h($category->created) ?></td>
-                    <td><?= h($category->modified) ?></td>
                     <td class="actions">
                         <?= $this->Html->link(__('View'), ['action' => 'view', $category->id]) ?>
+                        <!-- TODO: Only if admin -->
                         <?= $this->Html->link(__('Edit'), ['action' => 'edit', $category->id]) ?>
                         <?= $this->Form->postLink(
                             __('Delete'),
